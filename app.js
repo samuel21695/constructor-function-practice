c124cba978511715fb67df3a7835b9070210fe66
samuel21695 <samuel21695@gmail.com>
2023-10-25 23:16:40 +0900

    test

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ function Person(name, age) {
 // 'new' 키워드를 사용하면 생성자 함수를 이용, 객체를 만들 수 있다.
 const person1 = new Person("John", 30);
 const person2 = new Person("Alice, 25");
+console.log(person1);
+console.log(person2);
