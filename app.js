97541546fc5900b057ca24a34a60bbad1cca513c
samuel21695 <samuel21695@gmail.com>
2023-10-25 23:15:45 +0900

    use constructor function with the *new* keyword

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
+
+// 생성자 함수로 객체 만들기
+// 'new' 키워드를 사용하면 생성자 함수를 이용, 객체를 만들 수 있다.
+const person1 = new Person("John", 30);
+const person2 = new Person("Alice, 25");
